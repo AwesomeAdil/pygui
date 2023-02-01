103b33c2e0d99634f859909754ce9e14dec064e7
AwesomeAdil <84029606+AwesomeAdil@users.noreply.github.com>
2023-02-01 01:20:25 -0600

    initial commit with message sending

---
+++ sendText/gui.py
@@ -0,0 +1,67 @@
+import pyautogui
+import time
+import cv2 as cv 
+import numpy as np
+from screeninfo import get_monitors
+import sys
+
+branch = "Icons/"
+screenWidth, screenHeight = 3456, 2234 
+guiWidth, guiHeight = pyautogui.size()
+img1 = cv.imread(branch + 'Message.png')
+
+try:
+	corrected_img1 = cv.cvtColor(img1, cv.COLOR_RGB2BGR)
+except:
+	print("Message icon not recognized? Is it in Icons?")
+	exit()
+
+messageLocation = pyautogui.locateOnScreen(corrected_img1, confidence=.75)
+try:
+	messageLocationCenter = pyautogui.center(messageLocation) 
+except TypeError:
+	print('Could not find Message Icon, replace it in Icons or reduce confidence')
+	exit()
+
+messagex, messagey = messageLocationCenter.x, messageLocationCenter.y
+
+
+for i in range(3):
+	print(i+1)
+	time.sleep(1)
+
+
+pyautogui.moveTo(messagex/screenWidth * guiWidth, messagey/screenHeight * guiHeight, 2)
+pyautogui.click()
+
+time.sleep(0.5)
+
+path = branch+str(sys.argv[-1])+'.png'
+img2 = cv.imread(path)
+
+try:
+	corrected_img2 = cv.cvtColor(img2, cv.COLOR_RGB2BGR)
+except:
+        print("Contact not recognized? Is it in Contacts?")
+        exit()
+sentence = ' '.join(sys.argv[1:-1])
+
+contactLocation = pyautogui.locateOnScreen(corrected_img2, confidence=.7)
+
+try:
+	contactLocationCenter = pyautogui.center(contactLocation)
+except TypeError:
+        print('Could not find Contact Icon, replace it in Icons or reduce confidence')
+        exit()
+
+contactx, contacty = contactLocationCenter.x, contactLocationCenter.y
+pyautogui.moveTo(contactx/screenWidth * guiWidth, contacty/screenHeight * guiHeight, 2)
+pyautogui.click()
+
+pyautogui.write(sentence)
+choice = pyautogui.confirm(text='enter', title='Enter?', buttons=['OK', 'Cancel'])
+
+if choice == 'OK':
+	pyautogui.moveTo(contactx/screenWidth * guiWidth, contacty/screenHeight * guiHeight, 2)
+	pyautogui.click()
+	pyautogui.press('enter')
